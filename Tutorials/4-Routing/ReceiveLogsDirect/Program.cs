c04dbd18100336cda06cdeabacf70befef92ceb0
Isa <yurekliisa@gmail.com>
2020-10-02 02:39:45 +0300

    init routing subject projects

---
+++ Tutorials/4-Routing/ReceiveLogsDirect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiveLogsDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
