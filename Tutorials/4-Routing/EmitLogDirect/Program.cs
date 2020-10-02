df4359decae00dc9a99ad0a41df161119d7dc66a
İsa <yurekliisa@gmail.com>
2020-10-02 03:32:36 +0300

    Routing tutorial and fix errors on desktop

--- Tutorials/4-Routing/EmitLogDirect/Program.cs
+++ Tutorials/4-Routing/EmitLogDirect/Program.cs
@@ -2,11 +2,35 @@
 
 namespace EmitLogDirect
 {
+    // TODO : Change config 
+    // TODO : Fix error on desktop
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory(){
+                HostName="localhost"
+            };
+
+            using(var connection = factory.CreateConnection()){
+                using(var channel = connection.CreateModel()){
+                    channel.ExchangeDeclare(
+                        exchange:"direct_logs",
+                        type:"direct"
+                    );
+                    var severity = (args.Length > 0)? args[0] : "info";
+                    var message = (args.Length > 1) ? string.Join(" ",args.Skip(1).ToArray(): "Hello World!");
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(
+                        exchange:"direct_logs",
+                        routingKey:severity,
+                        basicProperties:null,
+                        body:body
+                    );
+                    System.Console.WriteLine("[X] Sent '{0}':'{1}'",severity,message);
+                }
+            }
+                Console.ReadLine();
         }
     }
 }
--- Tutorials/4-Routing/ReceiveLogsDirect/Program.cs
+++ Tutorials/4-Routing/ReceiveLogsDirect/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace ReceiveLogsDirect
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ReceiveLogsDirect {
+    class Program {
+        static void Main (string[] args) {
+            var factory = new ConnectionFactory () {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection ()) {
+                using (var channel = connection.CreateModel ()) {
+                    channel.ExchangeDeclare (
+                        exchange: "direct_logs",
+                        type: "direct"
+                    );
+                    var queueName = channel.QueueDeclare ().QueueName;
+                    if (args.Length < 1) {
+                        Console.Error.WriteLine ("Usage: {0} [Info] [Warning] [error]", Environment.GetCommandLineArgs () [0]);
+                        Console.ReadLine ();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+                    foreach (var severity in args) {
+                        channel.QueueBind (
+                            queue : queueName,
+                            exchange: "direct_logs",
+                            routingKey : severity
+                        );
+                    }
+                    Console.WriteLine ("[*] waiting for messages.");
+
+                    var consumer = new EventingBasicConsumer (channel);
+                    consumer.Received += (model, ea) {
+                        var body = ea.Body.ToArray ();
+                        var message = Encoding.UTF8.GetString (body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine ("[x] Received '{0}':'{1}'", routingKey, message);
+
+                    };
+                    channel.BasicConsume (queue: queueName,
+                        autoAck : true,
+                        consumer : consumer);
+
+                }
+            }
+            Console.ReadLine ();
         }
     }
 }
